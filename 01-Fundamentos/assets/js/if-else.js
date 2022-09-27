6a719ab2e632c1920f73a5fa8f3466f4bdbe43a5
Juan Martínez <jmartinezp1992@gmail.com>
2022-09-27 18:08:02 +0200

    Alternativa if-else Añadida

--- 01-Fundamentos/assets/js/if-else.js
+++ 01-Fundamentos/assets/js/if-else.js
@@ -19,3 +19,36 @@ if (dia == 27){ //Dobble igual comprueba, un igual asigna. Con tres igual, compr
 }else{
     console.log('No es Domingo');
 }
+
+// Alternativa a if else o switch, unicamente objetos
+
+dia = 3; //0:domingo. 1 lunes,...
+
+//Con objetos
+
+const diaLetras = {
+   0: 'domingo',
+   1: 'lunes',
+   2: 'martes',
+   3: 'miercoles',
+   4: 'jueves',
+   5: 'viernes',
+   6: 'sabado'
+}
+
+console.log(diaLetras[dia] || 'Dia no definido');
+
+
+// Con arrays
+
+const diaLetras2 = [
+    'domingo',
+    'lunes',
+    'martes',
+    'miercoles',
+    'jueves',
+    'viernes',
+    'sabado'
+];
+
+nsole.log(diaLetras2[dia]);
